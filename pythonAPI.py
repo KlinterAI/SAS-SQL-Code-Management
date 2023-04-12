6461798cfd3be756daa7cc8bcdc27eda09c6b99b
lilesmb <64851477+lilesmb@users.noreply.github.com>
2023-04-12 01:00:05 -0400

    API for SAS/SQL Database - *Has many bugs*

--- pythonAPI.py
+++ pythonAPI.py
@@ -2,10 +2,10 @@
 from flask import *
 import json, time, pandas as pd
 
-iot = pd.read_csv(r'KlinterAI/SAS-SQL-Code-Management/dataset/IoT.csv')
-iotToken = pd.read_csv(r'KlinterAI/SAS-SQL-Code-Management/dataset/iot_token.csv')
-iotMeta = pd.read_csv(r'KlinterAI/SAS-SQL-Code-Management/dataset/iot_metadata.csv')
-iotCost = pd.read_csv(r'KlinterAI/SAS-SQL-Code-Management/dataset/iot_cost_code.csv')
+iot = 'KlinterAI/SAS-SQL-Code-Management/dataset/IoT.csv'
+iotToken = 'KlinterAI/SAS-SQL-Code-Management/dataset/iot_token.csv'
+iotMeta = 'KlinterAI/SAS-SQL-Code-Management/dataset/iot_metadata.csv'
+iotCost = 'KlinterAI/SAS-SQL-Code-Management/dataset/iot_cost_code.csv'
 
 app = Flask(__name__)
 
@@ -17,17 +17,30 @@ def resultPage():
 
     return dumpData
 
-def runCSV(iot, iotToken, iotMeta, iotCost):
-    pass
+df = pd.read_csv('KlinterAI/SAS-SQL-Code-Management/dataset/iot_token.csv')
+
+def calcOpTime(df):
+    # Convert the 'start_time' and 'end_time' columns to datetime objects
+    id = df['id'] 
+    df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'])
+    timestamps_by_id = df.groupby('ID')['TIMESTAMP'].apply(list)
+
+    print(timestamps_by_id)
+
+iot_operational_time = calculate_operational_time()
+
+print('IoT Devices Operational Time:', iot_operational_time)
+
+def calcMatUse():
+    pass # Having trouble finding the data for the materials to even find out the material usage.
 
 @app.route('/user/', methods=['GET'])
 def requestPage():
     userRequest = str(request.args.get('user'))
-    data = {'Page': 'Results', 'Resulting Message': f'Fully loaded the \'Results\' page with user: {user}.'}
+    data = {'Page': 'Results', 'Resulting Message': f'Fully loaded the \'Results\' page with user: {userRequest}.'}
     dumpData = json.dumps(data)
 
     return dumpData
-
 
 
 if __name__ == '__main__':
